7f042540fe0e901f5faebc07d45cc9c1ec9ae4a1
tannhan-cell <nhanv1699@gmail.com>
2026-03-13 22:01:38 +0700

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -3,24 +3,26 @@ function createInvoice(){
 const { jsPDF } = window.jspdf
 const doc = new jsPDF()
 
-doc.text("HOA DON VOTANNHAN",20,20)
+doc.setFont("Helvetica")
+
+doc.text("HOA DON BY VOTANNHAN",20,20)
 
 let y=40
 let total=0
 
 cart.forEach(i=>{
 
-let line = i.name+" x"+i.qty+" = "+(i.qty*i.price)+" VND"
+let line=i.name+" x"+i.qty+" = "+(i.qty*i.price)+" VND"
 
 doc.text(line,20,y)
 
 y+=10
 
-total += i.qty*i.price
+total+=i.qty*i.price
 
 })
 
-doc.text("Tong: "+total+"đ",20,y+10)
+doc.text("Tong: "+total+" VND",20,y+10)
 
 doc.save("invoice.pdf")
 
