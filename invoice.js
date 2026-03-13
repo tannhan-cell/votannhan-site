825cf30f56350d27e5ce9d44edd9b62fac6c208a
tannhan-cell <nhanv1699@gmail.com>
2026-03-13 22:05:20 +0700

    Update invoice.js

--- invoice.js
+++ invoice.js
@@ -3,27 +3,55 @@ function createInvoice(){
 const { jsPDF } = window.jspdf
 const doc = new jsPDF()
 
-doc.setFont("Helvetica")
+// tiêu đề
+doc.setFont("Helvetica","bold")
+doc.setFontSize(18)
+doc.text("VOTANNHAN ELECTRONICS",105,20,null,null,"center")
 
-doc.text("HOA DON BY VOTANNHAN",20,20)
+doc.setFontSize(12)
+doc.setFont("Helvetica","normal")
+doc.text("Hoa don ban hang",105,30,null,null,"center")
 
-let y=40
+// đường kẻ
+doc.line(20,35,190,35)
+
+let y=50
 let total=0
 
-cart.forEach(i=>{
-
-let line=i.name+" x"+i.qty+" = "+(i.qty*i.price)+" VND"
-
-doc.text(line,20,y)
+doc.setFont("Helvetica","bold")
+doc.text("San pham",20,y)
+doc.text("SL",120,y)
+doc.text("Gia",150,y)
 
 y+=10
 
-total+=i.qty*i.price
+doc.setFont("Helvetica","normal")
+
+cart.forEach(i=>{
+
+let price=i.qty*i.price
+
+doc.text(i.name,20,y)
+doc.text(String(i.qty),120,y)
+doc.text(price+" VND",150,y)
+
+total+=price
+y+=10
 
 })
 
-doc.text("Tong: "+total+" VND",20,y+10)
+// đường kẻ
+doc.line(20,y,190,y)
 
-doc.save("invoice.pdf")
+y+=10
+
+doc.setFont("Helvetica","bold")
+doc.text("Tong cong: "+total+" VND",20,y)
+
+// footer
+doc.setFontSize(10)
+doc.text("Cam on quy khach da mua hang!",105,280,null,null,"center")
+
+doc.save("hoa-don-votannhan.pdf")
 
 }
