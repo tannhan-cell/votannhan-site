b4fe8535260ec02af5c88225ce11a47be4136116
tannhan-cell <nhanv1699@gmail.com>
2026-03-13 20:51:28 +0700

    Update shop-system.js

--- shop-system.js
+++ shop-system.js
@@ -1,24 +1,22 @@
-let cart=[]
+let cart = JSON.parse(localStorage.getItem("cart")) || []
 
 function addToCart(id){
 
-let p=products.find(x=>x.id===id)
+let p = products.find(x => x.id === id)
 
-let item=cart.find(x=>x.id===id)
+if(!p) return
+
+let item = cart.find(x => x.id === id)
 
 if(item){
-
 item.qty++
-
 }else{
-
 cart.push({
 id:p.id,
 name:p.name,
 price:p.price,
 qty:1
 })
-
 }
 
 updateCart()
@@ -28,21 +26,28 @@ updateCart()
 function updateCart(){
 
 let el=document.getElementById("cart-items")
-
 if(!el) return
 
 el.innerHTML=""
 
+let total=0
+
 cart.forEach((i,idx)=>{
+
+total += i.price*i.qty
 
 el.innerHTML += `
 <div>
-${i.name} x${i.qty}
+${i.name} x${i.qty} = ${i.price*i.qty}đ
 <button onclick="removeItem(${idx})">❌</button>
 </div>
 `
 
 })
+
+el.innerHTML += `<hr>Tổng: <b>${total}đ</b>`
+
+localStorage.setItem("cart",JSON.stringify(cart))
 
 }
 
@@ -53,3 +58,5 @@ cart.splice(i,1)
 updateCart()
 
 }
+
+updateCart()
